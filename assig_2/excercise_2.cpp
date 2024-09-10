ac955805dc49b0b99306cc47a4c1e58c5a3c1335
viwa3399 <viktor.wallsten.3399@student.uu.se>
2024-09-10 11:30:43 +0200

    fixat en seriell algoritm av uppgift 2

---
+++ assig_2/excercise_2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <chrono>
+
+int max;
+
+void SiveOfEratosthenes(int max){
+
+    //start with assuming every number is a prime
+    bool is_prime[max + 1]; // index of the prime number so needs to be of size n +1
+    for (int i = 0; i <= max+1; i++){
+        is_prime[i] = true;
+    }
+
+    for (int k = 2; k * k <= max; k++) {
+        //if a numer is a prime every multiple of that number is set to false
+            if (is_prime[k] == true) {
+                for (int i = k * k; i <= max; i += k)
+                    is_prime[i] = false;
+            }
+        }
+    
+    for (int p = 2; p <= max; p++){
+        if (is_prime[p])
+        {
+            std::cout << p << std::endl;
+        }
+    }   
+}
+
+void usage(char *program)
+{
+  std::cout << "Usage: " << program << " input a positiv integer" << std::endl;
+  exit(1);
+}
+
+int main(int argc, char *argv[]){
+
+    max = atoi(argv[1]);
+
+    if (argc != 2)
+    {
+      usage(argv[0]);
+    }
+
+    if (max <= 0)
+    {
+      usage(argv[0]);
+    }
+
+    std::cout << "starting timer!" << std::endl;
+    auto start_time = std::chrono::system_clock::now();
+
+    SiveOfEratosthenes(max);
+
+    std::chrono::duration<double> duration =
+    (std::chrono::system_clock::now() - start_time);
+  // *** timing ends here ***
+  std::cout << "Finished in " << duration.count() << " seconds (wall clock)." << std::endl;
+    return 0;
+}
