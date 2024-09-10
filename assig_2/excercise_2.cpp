e52d18b4935d4c44c99e7a68155cd923c8c30e50
bruce1887 <edvinbruce@hotmail.com>
2024-09-10 19:18:11 +0200

    shit works

--- assig_2/excercise_2.cpp
+++ assig_2/excercise_2.cpp
@@ -3,14 +3,16 @@
 #include <cassert>
 #include <cmath>
 #include <list>
+#include <mutex>
 
+std::mutex out;
 int max;
 
 #define MARKED true
 #define UNMARKED false
 
-int *numbers;
-bool *markers;
+int *numbers; // threads only read from this
+bool *markers; // threads only write to this
 int *primes_to_check;
 
 void usage(char *program)
@@ -23,7 +25,6 @@ void usage(char *program)
 void SequencialSive(int *numbers, int len)
 {
     bool is_prime[len] = {UNMARKED};
-
 
     // mark 0 and 1 if they are in the array
     if (len > 2)
@@ -78,9 +79,29 @@ void SequencialSive(int *numbers, int len)
     primes_to_check = primes;
 }
 
-void *ParallelSiveOfEratosthenes(void *data)
+void *ParallelSiveOfEratosthenes(void *ptr)
 {
-  
+    int *slice = reinterpret_cast<int *>(ptr);
+    int start_idx = slice[0];     // fetch start index
+    int end_idx = slice[1]; // fetch end index
+
+    // out.lock();
+    // std::cout << "Thread: " << pthread_self() << " start_idx: " << start_idx << " end_idx: " << end_idx << std::endl;
+    // out.unlock();
+
+    int *curr_prime = primes_to_check;
+
+    while(*curr_prime != 0){
+        for (int i = start_idx; i <= end_idx; i++)
+        {
+            if (numbers[i] % *curr_prime == 0)
+            {
+                markers[i] = MARKED;
+            }
+        }
+        curr_prime++;
+    }
+    return NULL;
 }
 
 int main(int argc, char *argv[])
@@ -89,8 +110,8 @@ int main(int argc, char *argv[])
     {
         usage(argv[0]);
     }
-    int num_threads = atoi(argv[1]);
-    max = atoi(argv[2]) + 1;
+    max = atoi(argv[1]);
+    int num_threads = atoi(argv[2]);
 
     if (max <= 0 || num_threads <= 0)
     {
@@ -114,26 +135,26 @@ int main(int argc, char *argv[])
 
     std::cout << "Fetching first primes up to sqrt_max..." << std::endl;
 
-    SequencialSive(numbers, sqrt_max +1);
+    SequencialSive(numbers, sqrt_max + 1);
 
-    int prime_idx = 0;
-    while (primes_to_check[prime_idx] != 0)
-    {
-        std::cout << primes_to_check[prime_idx] << std::endl;
-        prime_idx++;
-    }
-
+    // int prime_idx = 0;
+    // while (primes_to_check[prime_idx] != 0)
+    // {
+    //     std::cout << primes_to_check[prime_idx] << ",";
+    //     prime_idx++;
+    // }
+    // std::cout << std::endl;
     std::cout << "First primes fetched!" << std::endl;
-    exit(0);
 
     pthread_t threads[num_threads];
+
     int slice_size = (max - sqrt_max) / num_threads;
     int remainder = (max - sqrt_max) % num_threads;
 
     std::cout << "starting timer!" << std::endl;
     auto start_time = std::chrono::system_clock::now();
 
-    int slice_idx = 0;
+    int slice_idx = sqrt_max +1;
     for (int i = 0; i < num_threads; i++)
     {
         int *slice_arr = new int[2];
@@ -142,12 +163,30 @@ int main(int argc, char *argv[])
         if (i == num_threads - 1) // last thread gets the remainder
         {
             slice_arr[1] += remainder;
-        }
+        }        
         slice_idx += slice_size;
         void *ptr = slice_arr;
         pthread_create(&threads[i], NULL, ParallelSiveOfEratosthenes, ptr);
     }
 
+    // wait for all threads to finish
+    for (int i = 0; i < num_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    std::chrono::duration<double> duration =
+        (std::chrono::system_clock::now() - start_time);
+    // *** timing ends here ***
+    std::cout << "Finished in " << duration.count() << " seconds (wall clock)." << std::endl;
+    
+    // std::cout << "Primes: ";
+    // for (int i = 1; i < max; i++) {
+    //     if (markers[i] == UNMARKED) {
+    //         std::cout << numbers[i] << " ";
+    //     }
+    // }
+    std::cout << std::endl;
     // cleanup
     delete[] numbers;
     delete[] markers;
