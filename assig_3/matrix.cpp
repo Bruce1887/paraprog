a6ba1efbd552b1a5f4031fee6e6c2cfed185138a
viwa3399 <viktor.wallsten.3399@student.uu.se>
2024-10-09 14:55:21 +0200

    matrix

--- assig_3/matrix.cpp
+++ assig_3/matrix.cpp
@@ -1,10 +1,10 @@
 #include <omp.h>
 #include <chrono>
 #include <iostream>
+#include <cassert>
 
-
-void matrixMultiply_i(int dim, double** a, double** b, double** c) {
-    #pragma omp parallel for schedule(static) shared(a, b, c, dim) default(none) 
+void matrixMultiply_i(int dim, long double** a, long double** b, long double** c) {
+    #pragma omp parallel for schedule(static) shared(a, b, c, dim) 
     for (int i = 0; i < dim; i++) {
         for (int j = 0; j < dim; j++) {
             for (int k = 0; k < dim; k++) {
@@ -14,11 +14,11 @@ void matrixMultiply_i(int dim, double** a, double** b, double** c) {
     }
 }
 
-void matrixMultiply_ii(int dim, double** a, double** b, double** c) {
-    #pragma omp parallel for schedule(static) collapse(2) shared(a, b, c, dim) default(none)
+void matrixMultiply_ii(int dim, long double** a, long double** b, long double** c) {
+    #pragma omp parallel for schedule(static) collapse(2) shared(a, b, c, dim)
     for (int i = 0; i < dim; i++) {
         for (int j = 0; j < dim; j++) {
-            double sum = 0.0; 
+            long double sum = 0.0; 
             for (int k = 0; k < dim; k++) {
                 sum += a[i][k] * b[k][j];
             }
@@ -27,44 +27,59 @@ void matrixMultiply_ii(int dim, double** a, double** b, double** c) {
     }
 }
 
-void matrixMultiply_iii(int dim, double** a, double** b, double** c) {
-    #pragma omp parallel for schedule(static) collapse(3) shared(a, b, c, dim) default(none)
+void matrixMultiply_iii(int dim, long double** a, long double** b, long double** c) {
+    #pragma omp parallel for schedule(static) collapse(3) shared(a, b, c, dim)
     for (int i = 0; i < dim; i++) {
-        for (int k = 0; k < dim; k++) {
-            for (int j = 0; j < dim; j++) {
-                double temp = a[i][k] * b[k][j];
-                #pragma omp atomic
-                c[i][j] += temp;
+        for (int j = 0; j < dim; j++) {
+            for (int k = 0; k < dim; k++) {
+                long double temp = a[i][j] * b[j][k];
+                //#pragma omp atomic
+                c[i][k] += temp;
             }
         }
     }
 }
 
 //create matrix
-double** createMatrix(int rows, int cols) {
-    double** matrix = new double*[rows];
+long double** createMatrix(int rows, int cols) {
+    long double** matrix = new long double*[rows];
     for (int i = 0; i < rows; ++i) {
-        matrix[i] = new double[cols];
+        matrix[i] = new long double[cols];
         std::fill(matrix[i], matrix[i] + cols, 0.0);
     }
     return matrix;
 }
 
 //delete a matrix 
-void deleteMatrix(double** matrix, int rows) {
+void deleteMatrix(long double** matrix, int rows) {
     for (int i = 0; i < rows; ++i) {
         delete[] matrix[i];
     }
     delete[] matrix;
 }
 
-int main(){
-    int rows = 2048;
-    int cols = 2048;
+int main(int argc, char* argv[]) {
+    int rows;
+    int cols;
+    int num_threads;
+    if(argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <rows> <cols> <num_threads>" << std::endl;
+        return 1;
+    }
+    rows = atoi(argv[1]);
+    cols = atoi(argv[2]);
+    num_threads = atoi(argv[3]);
 
-    double** matrix_a = createMatrix(rows, cols);
-    double** matrix_b = createMatrix(rows, cols);
-    double** matrix_c = createMatrix(rows, cols);
+    omp_set_num_threads(num_threads);
+    assert(omp_get_max_threads() == num_threads);
+
+    long double** matrix_a = createMatrix(rows, cols);
+    long double** matrix_b = createMatrix(rows, cols);
+    long double** matrix_ci = createMatrix(rows, cols);
+    long double** matrix_cii = createMatrix(rows, cols);
+    long double** matrix_ciii = createMatrix(rows, cols);
+
+
 
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
@@ -72,10 +87,11 @@ int main(){
             matrix_b[i][j] = i * cols + j + 1; 
         }
     }
+    
     //i
     std::cout << "starting timer! for i)" << std::endl;
     auto start_time_1 = std::chrono::system_clock::now();
-    matrixMultiply_i(rows, matrix_a, matrix_b, matrix_c);
+    matrixMultiply_i(rows, matrix_a, matrix_b, matrix_ci);
     std::chrono::duration<double> duration_1 =
         (std::chrono::system_clock::now() - start_time_1);
     std::cout << "Finished in " << duration_1.count() << " seconds (wall clock)." << std::endl;
@@ -83,7 +99,7 @@ int main(){
     //ii
     std::cout << "starting timer! for ii)" << std::endl;
     auto start_time_2 = std::chrono::system_clock::now();
-    matrixMultiply_ii(rows, matrix_a, matrix_b, matrix_c);
+    matrixMultiply_ii(rows, matrix_a, matrix_b, matrix_cii);
     std::chrono::duration<double> duration_2 =
         (std::chrono::system_clock::now() - start_time_2);
     std::cout << "Finished in " << duration_2.count() << " seconds (wall clock)." << std::endl;
@@ -91,14 +107,38 @@ int main(){
     //iii
     std::cout << "starting timer! for iii)" << std::endl;
     auto start_time_3 = std::chrono::system_clock::now();
-    matrixMultiply_iii(rows, matrix_a, matrix_b, matrix_c);
+    matrixMultiply_iii(rows, matrix_a, matrix_b, matrix_ciii);
     std::chrono::duration<double> duration_3 =
         (std::chrono::system_clock::now() - start_time_3);
     std::cout << "Finished in " << duration_3.count() << " seconds (wall clock)." << std::endl;
     
+ 
+    long double threshold = 0.000001; // we are comparing floating point numbers, thus we allow for some error
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+           // std::cout << "[" << i << "," << j << "]" << std::endl;
+            if(abs(matrix_ci[i][j] - matrix_cii[i][j]) > threshold) {
+                std::cerr << "Matrices are not 1 and 2 equal! " << matrix_ci[i][j] << " != " << matrix_cii[i][j] << std::endl;
+                return 1;
+            }
+            if(abs(matrix_ci[i][j] - matrix_ciii[i][j]) > threshold) {
+                std::cerr << "Matrices are not 1 and 3 equal! " << matrix_ci[i][j] << " != " << matrix_cii[i][j] << std::endl;
+                return 1;
+            }
+            // if(matrix_ci[i][j] != matrix_ciii[i][j]) {
+            //     std::cout << "[i,j]: " << "[" << i << "," << j << "]"  << " diff: " << abs(matrix_ci[i][j] - matrix_cii[i][j])<< std::endl;
+            // }
+            // assert(matrix_ci[i][j] == matrix_cii[i][j]); // assert first is equal to second
+            // assert(matrix_ci[i][j] == matrix_ciii[i][j]); // assert first is equal to third
+        }
+    }
+    std::cout << "All asserts passed! All matrices are equal!" << std::endl;
+
     deleteMatrix(matrix_a, rows);
     deleteMatrix(matrix_b, rows);
-    deleteMatrix(matrix_c, rows);
+    deleteMatrix(matrix_ci, rows);
+    deleteMatrix(matrix_cii, rows);
+    deleteMatrix(matrix_ciii, rows);
 
     return 0;
 }
