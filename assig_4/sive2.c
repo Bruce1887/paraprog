8d644517bb43af450eed3a06c390051a1ecbf2e2
viwa3399 <viktor.wallsten.3399@student.uu.se>
2024-10-17 09:59:41 +0200

    sive med broadcast och reduce

--- assig_4/sive2.c
+++ assig_4/sive2.c
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size); 
 
-    int max = 1000;
+    int max = 10000;
 
     int sqrt_max = (int)sqrt(max) + 1;
 
